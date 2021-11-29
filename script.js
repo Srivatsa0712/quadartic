66b97270b3db501629cb1fa0391297c65f4d8c0f
Srivatsa Sugurkar <79782950+Srivatsa0712@users.noreply.github.com>
2021-11-29 20:12:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
-	
+use strict;
 function showData() 
 {
-var a=document.formone.a.value;
-var b=document.formone.b.value;
-var c=document.formone.c.value;
+    var a=document.formone.a.value;
+    var b=document.formone.b.value;
+    var c=document.formone.c.value;
 
 
-var aa = new Number(a);
-var bb = new Number(b);
-var cc = new Number(c);
+    var aa = new Number(a);
+    var bb = new Number(b);
+    var cc = new Number(c);
 
-var root1=new Number;
-var root2=new Number;
-var discr;
-discr=Math.pow(bb,2)-4*aa*cc;
-document.write("<h3>A="+aa+" ,B="+bb+" ,C="+cc+", Discriminant="+discr);
+    var root1=new Number;
+    var root2=new Number;
+    var discr;
+    discr=Math.pow(bb,2)-4*aa*cc;
+    alert("A="+aa+" B="+bb+" C="+cc+" Discriminant="+discr);
 
-if (discr>0)
-{
-root1=(-b+discr)/(2*aa);
-root2=(-b-discr)/(2*aa);
-document.write("<p>Roots of Quadratic Equation:</p>");
-document.write("<br>Root1="+root1+"<br>Root2="+root2+"</h3>");
-document.write("<hr>");
-}
-else
-{
-
-alert("Discriminant is less than zero. Complex, Conjugate roots");
-}
+    if (discr>0)
+    {
+        root1=(-b+discr)/(2*aa);
+        root2=(-b-discr)/(2*aa);
+        alert("Roots of Quadratic Equation:");
+        alert("Root1="+root1+" Root2="+root2+"");
+    }
+    else
+    {
+        alert("Discriminant is less than zero. Complex, Conjugate roots");
+    }
 }
